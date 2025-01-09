af8f810058abb49b711143e598c1fc5c26f1c4f3
Mosharof <md5470415@gmail.com>
2025-01-09 20:43:35 +0600

    react router setup app2 repo create

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>React router er loading setup</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Compo !!! </h1>
+        </div>
+    );
+};
+
+export default Home;
