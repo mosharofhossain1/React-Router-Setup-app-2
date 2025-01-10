d9bbb19996df166cb1146ebcc3e562000f864962
Mosharof <md5470415@gmail.com>
2025-01-10 15:57:44 +0600

    abou bloge and contact compo update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,17 @@
 const About = () => {
     return (
         <div>
-            <h1>About us compo ?? </h1>
+            <h1 className="text-2xl mt-5 mb-5 font-bold">Wellcom To About Section </h1>
+            <div className="grid grid-cols-2 gap-x-2 border-2 border-emerald-600 p-3">
+                <div className="border-2 p-3 border-indigo-600">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, blanditiis animi cumque nulla enim maxime laudantium qui harum pariatur saepe magni ducimus, molestias ad accusantium vero amet veritatis dolores? Officiis!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur perspiciatis tenetur aliquid repellendus sequi cumque veritatis sunt, labore, ex ad ab veniam accusantium natus deleniti neque quo corporis illo magnam.</p>
+                </div>
+                <div className="border-2 p-3 border-indigo-600">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla illo, modi aliquam rerum aliquid nihil recusandae perferendis error exercitationem ex, quos eaque ipsam perspiciatis nesciunt fugiat inventore, velit consectetur iste!</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla illo, modi aliquam rerum aliquid nihil recusandae perferendis error exercitationem ex, quos eaque ipsam perspiciatis nesciunt fugiat inventore, velit consectetur iste!</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,18 @@
 const Contact = () => {
     return (
         <div>
-            <h1>Contact us </h1>
+            <h1 className="text-2xl font-bold mb-5
+            ">Conected Us </h1>
+            <div className="flex flex-col bg-orange-500 p-5 space-y-3">
+                <input className="px-2 py-1
+                rounded-md  " type="text" placeholder="Enter Name" />
+                <input className="px-2 py-1
+                rounded-md" type="email" placeholder="Enter Email" />
+                <input className="px-2 py-1
+                rounded-md" type="password" placeholder="Enter Password " />
+                <button className="bg-gray-300 px-3 py-2
+                 text-black font-bold rounded-lg">Send Message </button>
+            </div>
         </div>
     );
 };
